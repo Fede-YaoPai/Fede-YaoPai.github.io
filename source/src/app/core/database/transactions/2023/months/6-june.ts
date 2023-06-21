ef1190f1a2daaa69e47cf70d138bc31cd6903fcd
Fede-YaoPai <federico.lecca@hotmail.com>
2023-06-21 22:41:45 +0200

    Added transactions for June 2023

---
+++ source/src/app/core/database/transactions/2023/months/6-june.ts
@@ -0,0 +1,15 @@
+import { Transaction } from "src/app/models/app.models";
+import { getUserBy } from "src/app/shared/utils/functions";
+import { UserNames } from "../../../users/users";
+import { USER_NOT_FOUND } from "src/app/models/app.constants";
+
+// This month I only paid Ale
+export const TRANSACTIONS_JUNE: Transaction[] = [
+  {
+    date: new Date(2023, (6 - 1), 15),
+    amount: 104,
+    sender: getUserBy('username', UserNames.Sfeduo)?.id || USER_NOT_FOUND,
+    recipient: getUserBy('username', UserNames.Aletta)?.id || USER_NOT_FOUND,
+    screenshot: 'ale-3.png'
+  }
+];
--- source/src/app/core/database/transactions/2023/transactions-2023.ts
+++ source/src/app/core/database/transactions/2023/transactions-2023.ts
@@ -1,9 +1,11 @@
 import { Transaction } from "src/app/models/app.models";
 import { TRANSACTIONS_APRIL } from "./months/4-april";
 import { TRANSACTIONS_MAY } from "./months/5-may";
+import { TRANSACTIONS_JUNE } from "./months/6-june";
 
 
 export const TRANSACTIONS_2023: Transaction[] = [
   ...TRANSACTIONS_APRIL,
-  ...TRANSACTIONS_MAY
+  ...TRANSACTIONS_MAY,
+  ...TRANSACTIONS_JUNE
 ];
