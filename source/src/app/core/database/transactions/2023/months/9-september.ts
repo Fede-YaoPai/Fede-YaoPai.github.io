d035f7353386050d4926a9e7c7500c7ee19fc9db
Fede-YaoPai <federico.lecca@hotmail.com>
2023-09-16 22:19:51 +0200

    Added September transaction for Ale

---
+++ source/src/app/core/database/transactions/2023/months/9-september.ts
@@ -0,0 +1,15 @@
+import { Transaction } from "src/app/models/app.models";
+import { getUserBy } from "src/app/shared/utils/functions";
+import { UserNames } from "../../../users/users";
+import { USER_NOT_FOUND } from "src/app/models/app.constants";
+
+// This month I only paid Ale
+export const TRANSACTIONS_SEPTEMBER_2023: Transaction[] = [
+  {
+    date: new Date(2023, (9 - 1), 16),
+    amount: 104,
+    sender: getUserBy('username', UserNames.Sfeduo)?.id || USER_NOT_FOUND,
+    recipient: getUserBy('username', UserNames.Aletta)?.id || USER_NOT_FOUND,
+    screenshot: 'ale-6.png'
+  }
+];
--- source/src/app/core/database/transactions/2023/transactions-2023.ts
+++ source/src/app/core/database/transactions/2023/transactions-2023.ts
@@ -4,6 +4,7 @@ import { TRANSACTIONS_MAY_2023 } from "./months/5-may";
 import { TRANSACTIONS_JUNE_2023 } from "./months/6-june";
 import { TRANSACTIONS_JULY_2023 } from "./months/7-july";
 import { TRANSACTIONS_AUGUST_2023 } from "./months/8-august";
+import { TRANSACTIONS_SEPTEMBER_2023 } from "./months/9-september";
 
 
 export const TRANSACTIONS_2023: Transaction[] = [
@@ -11,5 +12,6 @@ export const TRANSACTIONS_2023: Transaction[] = [
   ...TRANSACTIONS_MAY_2023,
   ...TRANSACTIONS_JUNE_2023,
   ...TRANSACTIONS_JULY_2023,
-  ...TRANSACTIONS_AUGUST_2023
+  ...TRANSACTIONS_AUGUST_2023,
+  ...TRANSACTIONS_SEPTEMBER_2023
 ];
