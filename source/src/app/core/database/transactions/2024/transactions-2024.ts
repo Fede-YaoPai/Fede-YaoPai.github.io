ad7f187df68ed52ea0e2dc3e03cd8c9aa48fe046
Fede-YaoPai <federico.lecca@hotmail.com>
2024-01-23 17:46:07 +0100

    Added transaction for January 2024 (only Aletta)

---
+++ source/src/app/core/database/transactions/2024/transactions-2024.ts
@@ -0,0 +1,7 @@
+import { Transaction } from "src/app/models/app.models";
+import { TRANSACTIONS_JANUARY_2024 } from "./months/1-january";
+
+
+export const TRANSACTIONS_2024: Transaction[] = [
+  ...TRANSACTIONS_JANUARY_2024
+];
--- source/src/app/core/database/transactions/transactions.ts
+++ source/src/app/core/database/transactions/transactions.ts
@@ -1,8 +1,10 @@
 import { Transaction } from "src/app/models/app.models";
 import { TRANSACTIONS_2023 } from "./2023/transactions-2023";
+import { TRANSACTIONS_2024 } from "./2024/transactions-2024";
 
 
 export const TRANSACTIONS: Transaction[] = [
-  ...TRANSACTIONS_2023
+  ...TRANSACTIONS_2023,
+  ...TRANSACTIONS_2024
 ];
 
