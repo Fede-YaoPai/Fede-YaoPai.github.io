2c16c010eb5b5d35f7fd92a967f5d57b122b3177
Fede-YaoPai <federico.lecca@hotmail.com>
2025-05-19 21:30:28 +0200

    Added transactions for May 2025.

--- source/src/app/core/database/transactions/2025/transactions-2025.ts
+++ source/src/app/core/database/transactions/2025/transactions-2025.ts
@@ -3,11 +3,13 @@ import { TRANSACTIONS_JANUARY_2025 } from "./months/1-january";
 import { TRANSACTIONS_FEBRUARY_2025 } from "./months/2-february";
 import { TRANSACTIONS_MARCH_2025 } from "./months/3-march";
 import { TRANSACTIONS_APRIL_2025 } from "./months/4-april";
+import { TRANSACTIONS_MAY_2025 } from "./months/5-may";
 
 
 export const TRANSACTIONS_2025: Transaction[] = [
   ...TRANSACTIONS_JANUARY_2025,
   ...TRANSACTIONS_FEBRUARY_2025,
   ...TRANSACTIONS_MARCH_2025,
-  ...TRANSACTIONS_APRIL_2025
+  ...TRANSACTIONS_APRIL_2025,
+  ...TRANSACTIONS_MAY_2025
 ];
